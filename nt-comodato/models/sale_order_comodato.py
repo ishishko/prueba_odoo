48a7119a864b1c6d5f18cdc719f9aea69177c0e8
Ignacio Shishko <shishkoignacio@gmail.com>
2024-05-29 13:30:08 -0300

    Corrigiendo version

--- nt-comodato/models/sale_order_comodato.py
+++ nt-comodato/models/sale_order_comodato.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models
+from odoo import fields, models ,api
 
 
 class SaleOrderComodato(models.Model):
     _inherit = "sale.order"
+
+    @api.constrains('comodato_id')
+    def _check_comodato_id(self):
+        for order in self:
+            if order.comodato_id:
+                if order.comodato_id.state != 'done':
+                    raise ValidationError(_('The comodato must be done')
