a3633335df321655a2847ea39559f576679aef4a
Ignacio Shishko <shishkoignacio@gmail.com>
2024-05-29 09:45:19 -0300

    Iniciando Modulo

---
+++ nt-comodato/__manifest___.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Comodato alquiler ",
+
+    'description': """
+Modulo para alquileor comodato.
+    """,
+
+    'website': "https://www.hitofusion.com/",
+    'summary': '',
+    'category': 'Sales/Sales',
+    'sequence': 160,
+    'version': '17.0',
+    'depends': ['sale'],
+    'data': [
+        'views/sale_order_views_comodato.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'license': 'AGPL-3',
+}
